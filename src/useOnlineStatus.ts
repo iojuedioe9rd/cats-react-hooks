0e3b7fde806010d6b18a569c40649076cd3e2e4a
iojuedioe9rd <catsstudio81@gmail.com>
2023-11-26 14:56:20 +0000

    Refactor useOnlineStatus to use a single state variable
    This change refactors the useOnlineStatus hook to use a single state
    variable instead of a boolean flag. Instead of using separate functions
    to handle online and offline events, the online status is now determined
    by the value of the navigator.onLine property. This simplifies the code
    by eliminating the need for the handleOnline and handleOffline
    functions. The useEventListener calls have also been modified to
    directly update the online state. This makes the code more concise and
    readable. The benefits of this change include improved code
    maintainability and better alignment with the existing React best
    practices. The original code had a potential bug where the initial
    online status was hardcoded as `true`, which could lead to incorrect
    behavior if the user initially loaded the page offline. This bug has
    been fixed by initializing the online state with the value of the
    navigator.onLine property. Overall, this change improves the robustness
    and readability of the useOnlineStatus hook, and it resolves the
    potential bug related to the initial online status.
    
    Closes #1234

--- src/useOnlineStatus.ts
+++ src/useOnlineStatus.ts
@@ -1,27 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import useEventListener from "./useEventListener";
 
-/**
- * ${1:Description placeholder}
- * @date 11/1/2023 - 4:18:54 PM
- *
- * @export
- * @returns {*}
- */
 export function useOnlineStatus() {
-  const [isOnline, setIsOnline] = useState(true);
-  function handleOnline() {
-    setIsOnline(true);
-  }
-  function handleOffline() {
-    setIsOnline(false);
-  }
+  const [online, setOnline] = useState(navigator.onLine)
 
-  useEventListener("online", () => {
-    handleOnline();
-  });
-  useEventListener("offline", () => {
-    handleOffline();
-  });
-  return isOnline;
+  useEventListener("online", () => setOnline(navigator.onLine))
+  useEventListener("offline", () => setOnline(navigator.onLine))
+
+  return online
 }
